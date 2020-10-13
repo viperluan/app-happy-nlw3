afefca0257df3392ee83217c15e18c065a60e723
viperluan <viperluan@gmail.com>
2020-10-13 16:28:57 -0300

    added some styles on landing-page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,28 @@ import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
 
+import logoImg from './images/logo.svg';
+
 function App() {
   return (
     <div id="page-landing">
-      <h1>Hello World</h1>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Itajaí</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="" className="enter-app">
+          >
+        </a>
+      </div>
     </div>
   );
 }
