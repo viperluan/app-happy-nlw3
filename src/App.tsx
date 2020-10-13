fdc87442906eedc0d16bc917ba40a6ffd839b016
viperluan <viperluan@gmail.com>
2020-10-13 00:49:04 -0300

    started landing-page css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
 function App() {
   return (
-    <div className="App">
+    <div id="page-landing">
       <h1>Hello World</h1>
     </div>
   );
